d466004b6f72e691b0a50287bb39d7281fd82b7c
xiaoxuan-fan <98197408+xiaoxuan-fan@users.noreply.github.com>
2022-11-09 16:54:49 -0500

    id test

--- IterativeDeepeningAI.py
+++ IterativeDeepeningAI.py
@@ -2,29 +2,34 @@
 # Date: 10/27/2022
 # Purpose: Iterative Deepening
 from AlphaBetaAI import AlphaBetaAI
-from math import inf
 import chess
-
+import time
+# changed branch
 
 class IterativeDeepeningAI:
-    def __init__(self, depth_limit):
+    def __init__(self, depth_limit, search_method, provided_time):
         self.depth_limit = depth_limit
-        self.ai = AlphaBetaAI(depth_limit)
+        self.search_method = search_method
+        self.provided_time = provided_time
 
     def choose_move(self, chess_board):
-        best_move = None
-        best_value = -inf
+        start_time = time.time()
+        move = None
+
         for curr_depth in range(self.depth_limit):
-            value, move = self.ai.alphabeta(chess_board, curr_depth, chess_board.turn)
-            if value > best_value:
-                best_value = value
-                best_move = move
-        print("IterativeDeepening AI recommending move " + str(best_move))
-        return best_move
+            ai = self.search_method(curr_depth)
+            move = ai.choose_move(chess_board)
+            current_time = time.time()
+            spent_time = current_time - start_time
+            if (self.provided_time - spent_time) < spent_time:
+                break
+
+        print("IterativeDeepening AI recommending move " + str(move))
+        return move
 
 
 if __name__ == '__main__':
     board = chess.Board()
     print(board)
-    ai = IterativeDeepeningAI(3)
+    ai = IterativeDeepeningAI(3, AlphaBetaAI, 3000)
     ai.choose_move(board)
